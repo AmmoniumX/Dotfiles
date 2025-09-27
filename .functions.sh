cbc92848fd3458e29b7e246211a5667da443299a
Diego Garcia <diego.garcia.cr@gmail.com>
2025-09-26 21:16:42 -0600

    add paru-install()

--- .functions.sh
+++ .functions.sh
@@ -133,3 +133,26 @@ function mysudo() {
   fi
 }
 
+# Fuzzy package search and install (source: https://github.com/basecamp/omarchy/blob/master/bin/omarchy-pkg-install)
+if command -v paru &> /dev/null; then
+  function paru-install() {
+    local fzf_args=(
+      --multi
+      --preview 'paru -Sii {1}'
+      --preview-label='alt-p: toggle description, alt-j/k: scroll, tab: multi-select, F11: maximize'
+      --preview-label-pos='bottom'
+      --preview-window 'down:65%:wrap'
+      --bind 'alt-p:toggle-preview'
+      --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up'
+      --bind 'alt-k:preview-up,alt-j:preview-down'
+      --color 'pointer:green,marker:green'
+    )
+    
+    local pkg_names=$(paru -Slq | fzf "${fzf_args[@]}")
+    
+    if [[ -n "$pkg_names" ]]; then
+      # Convert newline-separated selections to space-separated for yay
+      echo "$pkg_names" | tr '\n' ' ' | xargs paru -S
+    fi
+  }
+fi
