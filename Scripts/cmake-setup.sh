83842448e8822019a7fcc462bb41cffc3f37e408
Diego Garcia <diego.garcia.cr@gmail.com>
2026-02-28 17:34:02 -0600

    cmake-setup.sh: export and copy compile commands

--- Scripts/cmake-setup.sh
+++ Scripts/cmake-setup.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 cmake -S . -B build/debug -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug
-cmake -S . -B build/release -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build/release -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=On
+ln -s build/release/compile_commands.json .
