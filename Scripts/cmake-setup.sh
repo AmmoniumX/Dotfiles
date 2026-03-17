9da128532b6f967bf20c084d6615dc6110920df5
Diego Garcia <diego.garcia.cr@gmail.com>
2026-03-16 20:22:17 -0600

    improve cmake-setup.sh: use debug compile_commands, add vcpkg.cmake toolchain file

--- Scripts/cmake-setup.sh
+++ Scripts/cmake-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cmake -S . -B build/debug -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug
-cmake -S . -B build/release -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=On
-ln -s build/release/compile_commands.json .
+[[ -L compile_commands.json || -f compile_commands.json ]] && rm ./compile_commands.json
+cmake -S . -B build/debug -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+cmake -S . -B build/release -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+ln -s build/debug/compile_commands.json ./compile_commands.json
