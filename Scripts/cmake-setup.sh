e7601482742fda0a72fd2482aa0533492f08fb42
Diego Garcia <diego.garcia.cr@gmail.com>
2026-03-17 10:25:55 -0600

    cmake-setup.sh: early quit on any error

--- Scripts/cmake-setup.sh
+++ Scripts/cmake-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 [[ -L compile_commands.json || -f compile_commands.json ]] && rm ./compile_commands.json
 cmake -S . -B build/debug -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
 cmake -S . -B build/release -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
