69bc932a114c998f76159712867cdd66f3992e76
Diego Garcia <diego.garcia.cr@gmail.com>
2026-03-17 17:43:23 -0600

    update neovim configs, clean cmake-setup.sh

--- Scripts/cmake-setup.sh
+++ Scripts/cmake-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-[[ -L compile_commands.json || -f compile_commands.json ]] && rm ./compile_commands.json
 cmake -S . -B build/debug -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
 cmake -S . -B build/release -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+[[ -L compile_commands.json || -f compile_commands.json ]] && rm ./compile_commands.json
 ln -s build/debug/compile_commands.json ./compile_commands.json
