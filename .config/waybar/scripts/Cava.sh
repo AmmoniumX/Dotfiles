eac7f1120d9f84345bb76ba66d40e5c21b03f67f
Diego Garcia <diego.garcia.cr@gmail.com>
2026-07-20 16:55:36 -0600

    fix cava error on startup

--- .config/waybar/scripts/Cava.sh
+++ .config/waybar/scripts/Cava.sh
@@ -12,4 +12,7 @@ if pgrep -x cava >/dev/null; then
   done
 fi
 
-exec cava -p ~/.config/cava/config1 2>/dev/null | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
+while true; do
+  cava -p ~/.config/cava/config1 2>/dev/null
+  sleep 0.2
+done | grep --line-buffered -E '^[0-9;]+$' | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
